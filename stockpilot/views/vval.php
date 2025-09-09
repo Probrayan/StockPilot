3119235da25ab1944a4a8c2065d62af2da922d8b
GridTech95 <gridtech95@gmail.com>
2025-09-09 16:49:59 -0500

    Update vval.php

--- stockpilot/views/vval.php
+++ stockpilot/views/vval.php
@@ -9,24 +9,33 @@ require_once('controllers/cval.php');
     <div class="row">
         <div class="form-group col-md-6">
             <label for="nomval">Nombre del Valor</label>
-            <input type="text" name="nomval" id="nomval" class="form-control" value="<?php if($datOne && $datOne[0]['nomval']) echo $datOne[0]['nomval']; ?>">
+            <input type="text" name="nomval" id="nomval" class="form-control"  value="<?php if($datOne && $datOne[0]['nomval']) echo $datOne[0]['nomval']; ?>" required>
         </div>
 
                 <div class="form-group col-md-6">
                     <label for="iddom">Nombre del dominio</label>
                     <select name="iddom" id="iddom" class= "form-control form-select">
                     <?php if($datDom){ foreach($datDom AS $dd){ ?>
-                    <option value="<?=$dd['iddom']; ?>" <?php if($datOne && $datOne[0]['iddom']==$dd['iddom']) echo "selected"; ?>><?=$dd['nomdom'];?></option>
+                    <option value="<?=$dd['iddom']; ?>" <?php if($datOne && $datOne[0]['iddom']==$dd['iddom']) echo "selected"; ?>required><?=$dd['nomdom'];?> </option>
                     <?php }} ?>
                     </select>
                 </div>
+                <div class="form-group col-md-6">
+                <label for="codval">Codigo del valor</label>
+                <input type="text" name="codval" id="codval" class="form-control" value="<?php if($datOne && $datOne[0]['codval']) echo $datOne[0]['codval']; ?>" required>
+                </div>
 
-                                <div class="form-group col-md-12">
-                                <input type="hidden" name="idval" value="<?php if($datOne &&$datOne[0]['idval']) echo $datOne[0]['idval']; ?>">
+                        <div class="form-group col-md-6">
+                            <label for="fec_crea">Fecha de creación</label>
+                            <input type="date" name="fec_crea" id="fec_crea" class="form-control" value="<?php if($datOne && $datOne[0]['fec_crea']) echo $datOne[0]['fec_crea']; ?>" required>
+                        </div>
 
-                                <input type="hidden" name="ope" value="save">
+                            <div class="form-group col-md-12">
+                            <input type="hidden" name="idval" value="<?php if($datOne &&$datOne[0]['idval']) echo $datOne[0]['idval']; ?>">
+
+                            <input type="hidden" name="ope" value="save">
                                 <br>
-                                <input type="submit" class="form-control btn btn-dark" value="Enviar">
+                            <input type="submit" class="form-control btn btn-dark" value="Enviar">
                             </div>
     </div>    
 </form>
@@ -35,7 +44,9 @@ require_once('controllers/cval.php');
         <thead>
             <tr>
                 <th>Id y Nombre del valor</th>
-                <th>Id del dominio</th>
+                <th>Id del dominio y nombre del domino</th>
+                <th>Codigo del valor</th>
+                <th>Fecha de creación</th>
                 <th>Acciones</th>
                 <th></th> 
             </tr>
@@ -45,11 +56,13 @@ require_once('controllers/cval.php');
             <tr>
                 <td><?=$dt['idval']."-".$dt['nomval'];?></td>
                 <td><?=$dt['iddom'];?></td>
+                <td><?=$dt['codval'];?></td>
+                <td><?=$dt['fec_crea'];?></td>
                 <td>
-                    <a href="index.php?pg=<?=$pg;?>&idval=<?=$dt['idval'];?>&ope=edi" title="Editar">
+                    <a href="home.php?pg=<?=$pg;?>&idval=<?=$dt['idval'];?>&ope=edi" title="Editar">
                     <i class="fa-solid fa-pen-to-square fa-2x"></i>
                     </a>
-                    <a href="index.php?pg=<?=$pg;?>&idval=<?=$dt['idval'];?>&ope=eli" title="Eliminar" onclick="return eliminar();">
+                    <a href="home.php?pg=<?=$pg;?>&idval=<?=$dt['idval'];?>&ope=eli" title="Eliminar" onclick="return eliminar();">
                         <i class="fa-solid fa-trash-can fa-2x"></i>
                     </a>
                 </td>
@@ -60,9 +73,13 @@ require_once('controllers/cval.php');
             <tr>
                 <th>Id y Nombre del valor</th>
                 <th>Id del dominio</th>
+                <th>Codigo del valor</th>
+                <th>Fecha de creación</th>
                 <th>Acciones</th>
+                <th></th>   
                 <th></th> 
             </tr>
         </thead>
 
     </table>
+
